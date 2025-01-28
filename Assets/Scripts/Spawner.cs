7fe6759108fd3d259322b699828c42e41fdba78d
JiHun <pgh979@gmail.com>
2025-01-29 07:25:05 +0900

    Add Spawn Range

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -2,19 +2,23 @@ using UnityEngine;
 
 public class Spawner : MonoBehaviour
 {
+    [Header("Settings")]
+    public float minSpawnDelay;
+    public float maxSpawnDelay;
+    
     [Header("References")]
     public GameObject[] gameObjects;
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        Invoke("Spawn", 1f);
+        Invoke("Spawn", Random.Range(minSpawnDelay, maxSpawnDelay));
     }
 
     void Spawn()
     {
         GameObject randomObject = gameObjects[Random.Range(0, gameObjects.Length)];
         Instantiate(randomObject, transform.position, Quaternion.identity);
-        Invoke("Spawn", 2f);
+        Invoke("Spawn", Random.Range(minSpawnDelay, maxSpawnDelay));
     }
 }
