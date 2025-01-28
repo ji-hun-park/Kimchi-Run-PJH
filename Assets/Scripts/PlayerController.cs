0cbbfa73e6dbd731a6e1916cf81517987823e135
JiHun <pgh979@gmail.com>
2025-01-29 06:43:57 +0900

    Animation Complete

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,6 +8,10 @@ public class PlayerController : MonoBehaviour
     [Header("References")]
     public Rigidbody2D playerRigidBody;
     
+    public Animator playerAnimator;
+    
+    private bool isGrounded = true;
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -17,9 +21,23 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
         {
             playerRigidBody.AddForceY(jumpForce, ForceMode2D.Impulse);
+            isGrounded = false;
+            playerAnimator.SetInteger("State", 1);
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.name == "Platform")
+        {
+            if (!isGrounded)
+            {
+                playerAnimator.SetInteger("State", 2);
+            }
+            isGrounded = true;
         }
     }
 }
