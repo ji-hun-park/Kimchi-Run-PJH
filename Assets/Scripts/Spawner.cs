ba6a52ecc6391ccf0e272eeffeed8e822100fcb7
JiHun <pgh979@gmail.com>
2025-01-29 07:10:01 +0900

    Add Spawner

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -2,15 +2,19 @@ using UnityEngine;
 
 public class Spawner : MonoBehaviour
 {
+    [Header("References")]
+    public GameObject[] gameObjects;
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        Invoke("Spawn", 1f);
     }
 
-    // Update is called once per frame
-    void Update()
+    void Spawn()
     {
-        
+        GameObject randomObject = gameObjects[Random.Range(0, gameObjects.Length)];
+        Instantiate(randomObject, transform.position, Quaternion.identity);
+        Invoke("Spawn", 2f);
     }
 }
