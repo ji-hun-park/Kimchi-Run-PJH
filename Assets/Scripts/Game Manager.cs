ae2bd278b06857c6fb049f55d53647fc8735a61f
JiHun <pgh979@gmail.com>
2025-01-29 09:42:00 +0900

    Add Intro

--- Assets/Scripts/Game Manager.cs
+++ Assets/Scripts/Game Manager.cs
@@ -1,16 +1,50 @@
+using System;
 using UnityEngine;
+
+public enum GameState
+{
+    Intro,
+    Playing,
+    Dead
+}
 
 public class GameManager : MonoBehaviour
 {
+    public static GameManager instance;
+    
+    public GameState state = GameState.Intro;
+
+    [Header("References")]
+    
+    public GameObject IntroUI;
+    public GameObject enemySpawner;
+    public GameObject foodSpawner;
+    public GameObject goldSpawner;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+    }
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        IntroUI.SetActive(true);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (state == GameState.Intro && Input.GetKeyDown(KeyCode.Space))
+        {
+            state = GameState.Playing;
+            IntroUI.SetActive(false);
+            enemySpawner.SetActive(true);
+            foodSpawner.SetActive(true);
+            goldSpawner.SetActive(true);
+        }
     }
 }
